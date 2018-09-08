534e661cfb7b22e68218c702ff73c55b8a1f79d5
vavikast <vavikast@gmail.com>
2018-09-08 10:59:31 +0800

    txcredit

---
+++ txxy/txcredit/urls.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+from django.contrib import admin
+from django.urls import path
+from txcredit import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('home/', views.home, name='home'),
+    path('home1/', views.home1, name='home1'),
+    path('add/<int:a>/<int:b>/', views.add2, name='add2'),
+
+]
---
+++ txxy/txcredit/views.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+def home(request):
+    return render(request,'home.html')
+
+def home1(request):
+    return render(render,'home1.html',request.path)
+    #List = map(str, range(100))# 一个长度为100的 List
+    #return render(request, 'home1.html', {'List': List})
+
+
+def index(request):
+    return HttpResponse("欢迎来到天下信用")
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a)+int(b)
+    return HttpResponse(str(c))
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
